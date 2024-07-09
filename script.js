f7837e5ae1d62f4046ac668b83a7a5aec9084b42
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-07-09 01:58:13 -0500

    changes

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ let incomes = [];
 let expenses = [];
 let contextMenuItem = null;
 let resize = [0];
+let add = [true];
+
+let sizings = {0:'0', 1:'40', 2:'182.35'};
+let typeColors = {
+    'salary': '#00b3ff',
+    'gift': '#00ff15',
+    'business': '#cc00ff',
+    'interest': '#ffff00',
+    'custom': '#ff9900',
+    'other': '#2a2a2a',
+    'food': '#ffff00', //
+    'transport': '#cc00ff',
+    'fun': '#00b3ff',
+    'shopping': '#ff9900', //
+    'groceries': '#00ff15', // 
+    'other': '#2a2a2a', // 
+};
+
 
 // localStorage.clear();
 document.addEventListener('DOMContentLoaded', function() {
     // Load income and expenses from localStorage
     const storedIncomes = localStorage.getItem('incomes');
     const storedExpenses = localStorage.getItem('expenses');
-
     // Parse stored data if available
     if (storedIncomes) {
         incomes = JSON.parse(storedIncomes);
@@ -38,10 +55,19 @@ let touchStartTime = 0;
 const touchDuration = 400; 
 
 document.addEventListener('touchstart', function(event) {
-    console.log(event.target.classList)
     if (event.target.classList.contains('list-item')) {
         touchStartTime = Date.now();
     }
+
+    if (event.target.id == "income-header") {
+        let el = document.getElementById("income-header");
+        setTimeout(function() {
+            el.innerText = incomes.reduce((sum, income) => sum + income.amount, 0).toFixed(2);
+        }, 2000);
+        el.innerText = "Income";
+    }
+
+    // do it for expenses
 }, false);
 
 document.addEventListener('touchend', function(event) {
@@ -71,8 +97,9 @@ function addIncome() {
     if (desc && amount) {
         let now = new Date();
         let formattedTime = `${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getDate().toString().padStart(2, '0')}/${now.getFullYear()} ${now.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })}`;
-
-        const income = { id: Date.now(), time: formattedTime, desc, amount: parseFloat(amount) };
+        // resize 0 is deafault 1 is small and 2 is big
+        const income = { id: Date.now(), time: formattedTime, desc, amount: parseFloat(amount), type: typeIncome, hide: false, resize: 0};
+        typeIncome = 'other';
         incomes.push(income);
         // console.log(incomes);
         document.getElementById('income-desc').value = '';
@@ -89,7 +116,8 @@ function addExpense() {
     if (desc && amount) {
         let now = new Date();
         let formattedTime = `${(now.getMonth() + 1).toString().padStart(2, '0')}/${now.getDate().toString().padStart(2, '0')}/${now.getFullYear()} ${now.toLocaleString('en-US', { hour: 'numeric', minute: '2-digit', hour12: true })}`;
-        const expense = { id: Date.now(), time: formattedTime, desc, amount: parseFloat(amount) };
+        const expense = { id: Date.now(), time: formattedTime, desc, amount: parseFloat(amount), type: typeExpense, hide: false, resize: 0};
+        typeExpense = 'other';
         expenses.push(expense);
         document.getElementById('expense-desc').value = '';
         document.getElementById('expense-amount').value = '';
@@ -99,41 +127,53 @@ function addExpense() {
 }
 
 function setHeight(amount) {
-    return Math.min(1000, Math.max(5, (155-(resize[0]*15))*(amount*1.0/85)));
+    // return Math.min(1000, Math.max(5, (155-(resize[0]*15))*(amount*1.0/85)));
+    return Math.max(5, (155-(resize[0]*15))*(amount*1.0/85));
 }
 
 function updateIncomeList() {
     const incomeList = document.getElementById('income-list');
     incomeList.innerHTML = '';
     incomes.forEach(income => {
-        const item = document.createElement('div');
-        item.classList.add('list-item');
-        item.dataset.id = income.id;
-        item.dataset.type = 'income';
-
-        const height = setHeight(income.amount);
-
-        if (height > 35) {
-            item.innerHTML = `<span>${income.desc}</span>$${income.amount.toFixed(2)}</span>`;
-        } else {
-            // console.log(income.desc.length);
-            if (income.desc.length < 10 && height > 20) {
-                item.innerHTML = `<span>${income.desc}: $${income.amount.toFixed(2)}</span>`;
+        if (!income.hide) {
+            const item = document.createElement('div');
+            item.classList.add('list-item');
+            item.dataset.id = income.id;
+            item.dataset.type = 'income';
+            if (income.resize == 0) {
+                const height = setHeight(income.amount);
+                if (height > 35) {
+                    item.innerHTML = `<span>${income.desc}</span>$${income.amount.toFixed(2)}</span>`;
+                } else {
+                    if (income.desc.length < 10 && height > 20) {
+                        item.innerHTML = `<span>${income.desc}: $${income.amount.toFixed(2)}</span>`;
+                    } else {
+                        item.innerHTML = `<span>$${income.amount.toFixed(2)}</span>`;
+                    }
+                }
+                item.style.height = height + 'px';
+                if (height < 15) {
+                    item.style.fontSize = (item.style.height);
+                }
+            } else if (income.resize === 1) {
+                item.style.height = sizings[1] + 'px';
+                item.innerHTML = `<span>${income.desc}</span>$${income.amount.toFixed(2)}</span>`;
+            } else if (income.resize == 2) {
+                item.style.height = sizings[2] + 'px';
+                item.innerHTML = `<span>${income.desc}</span>$${income.amount.toFixed(2)}</span>`;
+                
+            }
+            // workk work work
+            item.style.setProperty('--triangle-color', typeColors[income.type]);
+            item.addEventListener('contextmenu', function(event) {
+                showContextMenu(event, item);
+            });
+            if (add[0]) {
+                incomeList.prepend(item);
             } else {
-                item.innerHTML = `<span>$${income.amount.toFixed(2)}</span>`;
+                incomeList.append(item);
             }
         }
-        
-        item.style.height = height + 'px';
-        if (height < 15) {
-            console.log(item.style.height);
-            item.style.fontSize = (item.style.height);
-        }
-        
-        item.addEventListener('contextmenu', function(event) {
-            showContextMenu(event, item);
-        });
-        incomeList.prepend(item);
     });
     updateBalance();
 }
@@ -142,46 +182,70 @@ function updateExpenseList() {
     const expenseList = document.getElementById('expense-list');
     expenseList.innerHTML = '';
     expenses.forEach(expense => {
-        const item = document.createElement('div');
-        item.classList.add('list-item');
-        item.dataset.id = expense.id;
-        item.dataset.type = 'expense';
+        if (!expense.hide) {
+            const item = document.createElement('div');
+            item.classList.add('list-item');
+            item.dataset.id = expense.id;
+            item.dataset.type = 'expense';
 
-        const height = setHeight(expense.amount);
+            if (expense.resize == 0) {
+                const height = setHeight(expense.amount);
 
-        // expense.amount >= 20 && height > 35
-        if (height > 35) {
-            item.innerHTML = `<span>${expense.desc}</span>$${expense.amount.toFixed(2)}</span>`;
-        } else {
-            if (expense.desc.length < 10 && height > 20) {
-                item.innerHTML = `<span>${expense.desc}: $${expense.amount.toFixed(2)}</span>`;
+            if (height > 35) {
+                item.innerHTML = `<span>${expense.desc}</span>$${expense.amount.toFixed(2)}</span>`;
             } else {
-                item.innerHTML = `<span>$${expense.amount.toFixed(2)}</span>`;
+                if (expense.desc.length < 10 && height > 20) {
+                    item.innerHTML = `<span>${expense.desc}: $${expense.amount.toFixed(2)}</span>`;
+                } else {
+                    item.innerHTML = `<span>$${expense.amount.toFixed(2)}</span>`;
+                }
+            }
+            item.style.height = height + 'px';
+            if (height < 15) {
+                item.style.fontSize = (item.style.height);
+            }
+            } else if (expense.resize === 1) {
+                item.style.height = sizings[1] + 'px';
+                item.innerHTML = `<span>${expense.desc}</span>$${expense.amount.toFixed(2)}</span>`;
+            } else if (expense.resize == 2) {
+                item.style.height = sizings[2] + 'px';
+                item.innerHTML = `<span>${expense.desc}</span>$${expense.amount.toFixed(2)}</span>`;
+            }
+
+            item.style.setProperty('--triangle-color', typeColors[expense.type]);
+            item.addEventListener('contextmenu', function(event) {
+                showContextMenu(event, item);
+            });
+            if (add[0]) {
+                expenseList.prepend(item);
+            } else {
+                expenseList.append(item);
             }
         }
-        item.style.height = height + 'px';
-        if (height < 15) {
-            console.log(item.style.height);
-            item.style.fontSize = (item.style.height);
-        }
-        item.addEventListener('contextmenu', function(event) {
-            showContextMenu(event, item);
-        });
-        expenseList.prepend(item);
     });
     updateBalance();
 }
 
-function resizeDown() {
-    resize[0] += 1;
+function update() {
     updateIncomeList();
     updateExpenseList();
 }
 
+function handleOrder(state) {
+    add[0] = state;
+    update()
+}
+
+// handleOrder(true);
+
+function resizeDown() {
+    resize[0] += 1;
+    update();
+}
+
 function resizeUp() {
     resize[0] -= 1;
-    updateIncomeList();
-    updateExpenseList();
+    update();
 }
 
 function showContextMenu(event, item) {
@@ -194,48 +258,50 @@ function showContextMenu(event, item) {
 }
 
 function handleContextAction(action) {
-    // if (!contextMenuItem) return;
-    // alert(action);  
-    // alert(contextMenuItem);
     const id = parseInt(contextMenuItem.dataset.id);
     const type = contextMenuItem.dataset.type;
-    // alert(type);
-
+    const item = type === 'income' ? incomes.find(income => income.id === id) : expenses.find(expense => expense.id === id);
     if (action === 'delete') {
         if (type === 'income') {
             incomes = incomes.filter(income => income.id !== id);
-            updateIncomeList();
-            saveData();
         } else if (type === 'expense') {
             expenses = expenses.filter(expense => expense.id !== id);
-            updateExpenseList();
-            saveData();
         }
     } else if (action === 'info') {
-        const item = type === 'income' ? incomes.find(income => income.id === id) : expenses.find(expense => expense.id === id);
-        alert(`INFO:\nTime: ${item.time}\nDescription: ${item.desc}\nAmount: $${item.amount.toFixed(2)}`);
+        alert(`INFO:\nTime: ${item.time}\nDescription: ${item.desc}\nAmount: $${item.amount.toFixed(2)}\nType: ${item.type}`);
     } else if (action === 'edit') {
-        const item = type === 'income' ? incomes.find(income => income.id === id) : expenses.find(expense => expense.id === id);
         const newDesc = prompt('Enter new description:', item.desc);
         if (!newDesc) return;
         const newAmount = prompt('Enter new amount:', item.amount);
-        // console.log(newDesc, newAmount, newDesc && !isNaN(newAmount));
+        // const newType = prompt(`Enter new type: (${incomeTypes.join(', ')})`, item.type);
+        // newType = incomeTypes.includes(newType) ? newType : 'other';
         if ((newDesc && !isNaN(newAmount)) || newAmount==null) {
             if (type === 'income') {
+                let newType = prompt(`Enter new type: (${incomeTypes.join(', ')})`, item.type);
+                newType = incomeTypes.includes(newType) ? newType : 'other';
                 const income = incomes.find(income => income.id === id);
                 income.desc = newDesc;
                 income.amount = newAmount ? parseFloat(newAmount) : income.amount;
-                updateIncomeList();
+                income.type = newType
             } else if (type === 'expense') {
+                let newType = prompt(`Enter new type: (${expenseTypes.join(', ')})`, item.type);
+                newType = expenseTypes.includes(newType) ? newType : 'other';
                 const expense = expenses.find(expense => expense.id === id);
                 expense.desc = newDesc;
                 expense.amount = newAmount ? parseFloat(newAmount) : expense.amount;
-                updateExpenseList();
+                expense.type = newType;
             }
-            updateBalance();
-            saveData();
         }
+    } else if (action === 'hide') {
+        item.hide = true;
+    } else if (action === 'resizesmall') {
+        item.resize = 1;
+    } else if (action === 'resizebig') {
+        item.resize = 2;
     }
+    updateBalance();
+    saveData();
+    update();
     document.getElementById('context-menu').style.display = 'none';
 }
 
@@ -248,8 +314,143 @@ function updateBalance() {
         document.getElementById('balance').style.color =  "#ff000d";
         document.getElementById('balance').style.borderColor =  "#ff000d";
     }else{
-        // #39ff14
-        document.getElementById('balance').style.color =  "#39ff14";
-        document.getElementById('balance').style.borderColor =  "#39ff14";
+        document.getElementById('balance').style.color =  "#00ff33";
+        document.getElementById('balance').style.borderColor =  "#00ff33";
     }
 }
+
+let timeoutId1;
+let timeoutId2;
+
+document.getElementById("income-header").addEventListener('mouseenter', function(event) {
+    clearTimeout(timeoutId1);
+    let el = document.getElementById("income-header");
+    el.innerText = incomes.reduce((sum, income) => sum + income.amount, 0).toFixed(2);
+}, false);
+
+document.getElementById("income-header").addEventListener('mouseleave', function(event) {
+    timeoutId1 = setTimeout(function() {
+        let el = document.getElementById("income-header");
+        el.innerText = "Income";
+    }, 2000);
+}, false);
+
+document.getElementById("income-header").addEventListener('click', function(event) {
+    clearTimeout(timeoutId1);
+}, false);
+
+document.getElementById("expense-header").addEventListener('mouseenter', function(event) {
+    clearTimeout(timeoutId2);
+    let el = document.getElementById("expense-header");
+    el.innerText = expenses.reduce((sum, expense) => sum + expense.amount, 0).toFixed(2);
+}, false);
+
+document.getElementById("expense-header").addEventListener('mouseleave', function(event) {
+    timeoutId2 = setTimeout(function() {
+        let el = document.getElementById("expense-header");
+        el.innerText = "Expenses";
+    }, 2000);
+}, false);
+
+document.getElementById("expense-header").addEventListener('click', function(event) {
+    clearTimeout(timeoutId1);
+}, false);
+
+document.getElementById("balance").addEventListener("click", function(event) {
+    showAllIncome();
+    showAllExpenses(); 
+    showActualSizesIncomes();
+    showActualSizesExpenses();
+}, false)
+
+
+function showAllIncome() {
+    incomes.forEach(income => {
+        income.hide = false;
+    });
+    update();
+    saveData();
+}
+
+function showAllExpenses() {
+    expenses.forEach(expense => {
+        expense.hide = false;
+    });
+    update();
+    saveData();
+}
+
+function showActualSizesIncomes() {
+    incomes.forEach(income => {
+        income.resize = 0;
+    });
+    update();
+    saveData();
+}
+
+function showActualSizesExpenses() {
+    expenses.forEach(expense => {
+        expense.resize = 0;
+    });
+    update();
+    saveData();
+}
+
+function handleOPExpense() {
+    document.getElementById('popup2').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function handleOPIncome() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+
+let typeIncome = 'other';
+let incomeTypes = ['salary', 'gift', 'business', 'interest', 'custom', 'other'];
+let typeExpense = 'other';
+let expenseTypes = ['food', 'transport', 'fun', 'shopping', 'custom', 'other'];
+document.querySelectorAll('.selectable-option').forEach(function(option) {
+    option.addEventListener('click', function() {
+        if (incomeTypes.includes(this.getAttribute('data-value'))) {
+            typeIncome = this.getAttribute('data-value');
+        } else {
+            typeExpense = this.getAttribute('data-value');
+        }
+        document.getElementById('popup').style.display = 'none';
+        document.getElementById('popup2').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+    });
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+});
+
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('popup2').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+});
+
+function ensureAllProperties(array) {
+    const defaultValues = {
+        type: 'other',
+        hide: false,
+        resize: 0
+    };
+
+    array.forEach(item => {
+        for (const key in defaultValues) {
+            if (!item.hasOwnProperty(key)) {
+                item[key] = defaultValues[key];
+            }
+        }
+    });
+
+    return array;
+}
+
+ensureAllProperties(incomes);
+ensureAllProperties(expenses);
